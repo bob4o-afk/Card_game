4be99f33adbc3f9050c430e3df4b1aa8a0dd2348
yoni storaro <73361803+yonichu@users.noreply.github.com>
2023-06-08 21:11:17 +0300

    Update karti.c
    adding comments and descriptions to the code

--- karti.c
+++ karti.c
@@ -17,6 +17,7 @@ struct Player {
     int numCards;
 };
 
+//comparison function for sorting cards
 int compareCards(const void *a, const void *b) {
     const struct Card *cardA = (const struct Card *)a;
     const struct Card *cardB = (const struct Card *)b;
@@ -39,17 +40,20 @@ int main() {
 
     printf("Enter a file name to open: ");
     scanf("%s", filename);
-
+    
+    //open the file for reading
     file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
         return 1;
     }
-
+    
+    //initialize the players hand count
     for (i = 0; i < NUM_PLAYERS; i++) {
         players[i].numCards = 0;
     }
-
+    
+    //read card values from the file into the deck
     for (i = 0; i < NUM_CARDS; i++) {
         char value[3];
         fscanf(file, "%s %c", value, &deck[i].suit);
@@ -57,12 +61,14 @@ int main() {
     }
     fclose(file);
 
+    //loop to continue until a winner is found and all cards are played
     while (winner == -1 && turn < NUM_CARDS) {
         struct Card currentCard = deck[turn];
         struct Player *currentPlayer = &players[turn % NUM_PLAYERS];
         currentPlayer->hand[currentPlayer->numCards] = currentCard;
         currentPlayer->numCards++;
-
+         
+        //checks if the current player has 4 cards that are the same
         if (currentPlayer->numCards >= 4) {
           qsort(currentPlayer->hand, currentPlayer->numCards, sizeof(struct Card), compareCards);
 
@@ -76,7 +82,8 @@ int main() {
 
         turn++;
     }
-
+    
+    //if no winner is found after all cards are played determine the winner based on the highest total/suit strength
     if (winner == -1) {
         int maxTotal = -1;
         int maxSuitStrength = -1;
@@ -85,14 +92,16 @@ int main() {
             struct Player *currentPlayer = &players[i];
             int total = 0;
             int suitStrength = -1;
-
+            
+            // calculate the total value of the hand and the highest suit
             for (j = 0; j < currentPlayer->numCards; j++) {
                 total += currentPlayer->hand[j].value;
                 if (currentPlayer->hand[j].suit > suitStrength) {
                     suitStrength = currentPlayer->hand[j].suit;
                 }
             }
-
+            
+            //update the winner if the current player has higher total and suit strenght than the previous
             if (total > maxTotal || (total == maxTotal && suitStrength > maxSuitStrength)) {
                 winner = i;
                 maxTotal = total;
@@ -100,7 +109,8 @@ int main() {
             }
         }
     }
-
+    
+    //print the winning player or no winner in some cases
     if (winner != -1) {
         printf("Player %d wins: ", winner + 1);
 
