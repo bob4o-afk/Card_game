4132ade8ad2bc4e0e425784f12b856f0ee68dff8
bobinkata <80552018+bob4o-afk@users.noreply.github.com>
2023-06-09 09:03:01 +0300

    Update blowfish_algorithm.h
    adding declarations

--- blowfish_algorithm.h
+++ blowfish_algorithm.h
@@ -1,1 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
 
+void blowfish_encrypt(uint32_t *xl, uint32_t *xr, const uint32_t *P,const uint32_t (*S)[256]);
+void blowfish_key_schedule(uint32_t *P, uint32_t *S, const uint8_t *key,size_t key_len);
+uint32_t Feistel(uint32_t x, const uint32_t (*S)[256]);
+void encrypt_file(const char *input_file, const char *output_file, const char *key);
